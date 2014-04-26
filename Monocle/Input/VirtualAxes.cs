ac4a9a3130c82ba4e2b9280c698c043987c7e5ea
Mattbook <Mattbook@localhost>
2014-04-25 23:48:19 -0700

    Added Virtual Buttons and Axes for input

---
+++ Monocle/Input/VirtualAxes.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monocle
+{
+    public class VirtualAxes
+    {
+        public List<VirtualAxesNode> Nodes;
+
+        public VirtualAxes()
+        {
+            Nodes = new List<VirtualAxesNode>();
+        }
+
+        public VirtualAxes(params VirtualAxesNode[] nodes)
+            : this()
+        {
+            foreach (var node in nodes)
+                Nodes.Add(node);
+        }
+
+        public Vector2 Value
+        {
+            get
+            {
+                Vector2 value = Vector2.Zero;
+
+                foreach (var node in Nodes)
+                {
+                    Vector2 nodeValue = node.Value;
+                    if (nodeValue != Vector2.Zero)
+                    {
+                        value = nodeValue;
+                        break;
+                    }
+                }
+
+                return value;
+            }
+        }
+    }
+
+    public abstract class VirtualAxesNode
+    {
+        public abstract Vector2 Value { get; }
+    }
+
+    public class VirtualAxesPadLeftStick : VirtualAxesNode
+    {
+        public int GamepadIndex;
+        public float Deadzone;
+
+        public VirtualAxesPadLeftStick(int gamepadIndex, float deadzone)
+        {
+            GamepadIndex = gamepadIndex;
+            Deadzone = deadzone;
+        }
+
+        public override Vector2 Value
+        {
+            get
+            {
+                return MInput.GamePads[GamepadIndex].GetLeftStick(Deadzone);
+            }
+        }
+    }
+
+    public class VirtualAxesKeys : VirtualAxesNode
+    {
+        public enum OverlapBehaviors { CancelOut, TakeOlder, TakeNewer };
+
+        public OverlapBehaviors OverlapBehavior;
+        public Keys Left;
+        public Keys Right;
+        public Keys Up;
+        public Keys Down;
+
+        private Vector2 previousValue;
+
+        public VirtualAxesKeys(OverlapBehaviors overlapBehavior, Keys left, Keys right, Keys up, Keys down)
+        {
+            OverlapBehavior = overlapBehavior;
+            Left = left;
+            Right = right;
+            Up = up;
+            Down = down;
+        }
+
+        public override Vector2 Value
+        {
+            get
+            {
+                Vector2 value = Vector2.Zero;
+
+                //X Axis
+                if (MInput.Keyboard.Check(Left))
+                {
+                    if (MInput.Keyboard.Check(Right))
+                    {
+                        switch (OverlapBehavior)
+                        {
+                            case OverlapBehaviors.CancelOut:
+                                value.X = 0;
+                                break;
+
+                            case OverlapBehaviors.TakeNewer:
+                                value.X = -previousValue.X;
+                                break;
+
+                            case OverlapBehaviors.TakeOlder:
+                                value.X = previousValue.X;
+                                break;
+                        }
+                    }
+                    else
+                        value.X = -1;
+                }
+                else if (MInput.Keyboard.Check(Right))
+                    value.X = 1;
+                else
+                    value.X = 0;
+
+                //Y Axis
+                if (MInput.Keyboard.Check(Up))
+                {
+                    if (MInput.Keyboard.Check(Down))
+                    {
+                        switch (OverlapBehavior)
+                        {
+                            case OverlapBehaviors.CancelOut:
+                                value.Y = 0;
+                                break;
+
+                            case OverlapBehaviors.TakeNewer:
+                                value.Y = -previousValue.Y;
+                                break;
+
+                            case OverlapBehaviors.TakeOlder:
+                                value.Y = previousValue.Y;
+                                break;
+                        }
+                    }
+                    else
+                        value.Y = -1;
+                }
+                else if (MInput.Keyboard.Check(Down))
+                    value.Y = 1;
+                else
+                    value.Y = 0;
+
+                previousValue = value;
+                return value;
+            }
+        }
+    }
+}
---
+++ Monocle/Input/VirtualButton.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monocle
+{
+    public class VirtualButton
+    {
+        public List<VirtualButtonNode> Nodes { get; private set; }
+
+        public VirtualButton()
+        {
+            Nodes = new List<VirtualButtonNode>();
+        }
+
+        public VirtualButton(params VirtualButtonNode[] nodes)
+            : this()
+        {
+            foreach (var node in nodes)
+                Nodes.Add(node);
+        }
+
+        public bool Check
+        {
+            get
+            {
+                foreach (var node in Nodes)
+                    if (node.Check)
+                        return true;
+                return false;
+            }
+        }
+
+        public bool Pressed
+        {
+            get
+            {
+                foreach (var node in Nodes)
+                    if (node.Pressed)
+                        return true;
+                return false;
+            }
+        }
+
+        public bool Released
+        {
+            get
+            {
+                foreach (var node in Nodes)
+                    if (node.Released)
+                        return true;
+                return false;
+            }
+        }
+    }
+
+    public abstract class VirtualButtonNode
+    {
+        public abstract bool Check { get; }
+        public abstract bool Pressed { get; }
+        public abstract bool Released { get; }
+    }
+
+    public class VirtualButtonKey : VirtualButtonNode
+    {
+        public Keys Key;
+
+        public VirtualButtonKey(Keys key)
+        {
+            Key = key;
+        }
+
+        public override bool Check
+        {
+            get { return MInput.Keyboard.Check(Key); }
+        }
+
+        public override bool Pressed
+        {
+            get { return MInput.Keyboard.Pressed(Key); }
+        }
+
+        public override bool Released
+        {
+            get { return MInput.Keyboard.Released(Key); }
+        }
+    }
+
+    public class VirtualButtonPadButton : VirtualButtonNode
+    {
+        public int GamepadIndex;
+        public Buttons Button;
+
+        public VirtualButtonPadButton(int gamepadIndex, Buttons button)
+        {
+            GamepadIndex = gamepadIndex;
+            Button = button;
+        }
+
+        public override bool Check
+        {
+            get { return MInput.GamePads[GamepadIndex].Check(Button); }
+        }
+
+        public override bool Pressed
+        {
+            get { return MInput.GamePads[GamepadIndex].Pressed(Button); }
+        }
+
+        public override bool Released
+        {
+            get { return MInput.GamePads[GamepadIndex].Released(Button); }
+        }
+    }
+}
