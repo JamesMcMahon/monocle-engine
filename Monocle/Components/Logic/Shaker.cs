9ac4f1853fdba111d2ff418e269b90418c9e8ad6
MattBook <mattmakesgames@gmail.com>
2016-03-07 23:08:55 -0800

    Shaker component

---
+++ Monocle/Components/Logic/Shaker.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monocle
+{
+    public class Shaker : Component
+    {
+        public Vector2 Value;
+        public float Interval = .05f;
+        public float Timer;
+
+        private bool on;
+
+        public Shaker(bool on = true)
+            : base(true, false)
+        {
+            this.on = on;
+        }
+
+        public bool On
+        {
+            get
+            {
+                return on;
+            }
+
+            set
+            {
+                on = value;
+                if (!on)
+                    Timer = 0;
+            }
+        }
+
+        public void ShakeFor(float seconds)
+        {
+            on = true;
+            Timer = seconds;
+        }
+
+        public override void Update()
+        {
+            if (on && Timer > 0)
+            {
+                Timer -= Engine.DeltaTime;
+                if (Timer <= 0)
+                    on = false;
+            }
+
+            if (on)
+            {
+                if (Entity.Scene.OnInterval(Interval))
+                    Value = Calc.Random.ShakeVector();
+            }
+            else
+                Value = Vector2.Zero;
+        }
+    }
+}
